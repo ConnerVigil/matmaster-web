9287953c725d5d1f7236eca208768175b710a759
Conner Vigil <cjvigil@live.com>
2024-09-21 13:10:24 -0600

    saving work

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,54 @@
 import React from "react";
 import Image from "next/image";
 import { FaFacebook } from "react-icons/fa";
-import { FaX, FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <div className="bg-white text-textPrimary h-32">
-      <div className="flex flex-col px-4 py-6">
-        <div className="flex items-center space-x-4 mb-2">
-          <div className="w-8 h-8 relative">
-            <Image
-              src="/MatMaster-60x60.svg"
-              alt="Company Logo"
-              layout="fill"
-              objectFit="contain"
-            />
+    <div>
+      <div className="bg-white text-textPrimary h-auto">
+        <div className="flex justify-between px-4 py-6">
+          <div className="flex flex-col">
+            <div className="flex items-center space-x-4 mb-2">
+              <div className="w-8 h-8 relative">
+                <Image
+                  src="/MatMaster-60x60.svg"
+                  alt="Company Logo"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+              <span className="text-lg font-semibold">MatMaster</span>
+            </div>
+            <span className="text-sm text-gray-500">
+              From setup to completion, MatMaster handles the details so you can
+              focus on wrestling.
+            </span>
           </div>
-          <span className="text-lg font-semibold">MatMaster</span>
+          <div className="flex flex-col items-start">
+            <span className="text-sm font-semibold mb-2 text-primary">
+              Get the app
+            </span>
+            <div className="flex flex-col space-y-2">
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <Image
+                  src="/AppleStoreBadge.svg"
+                  alt="Download on the App Store"
+                  width={120}
+                  height={40}
+                />
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <Image
+                  src="/GooglePlayBadge.png"
+                  alt="Get it on Google Play"
+                  width={135}
+                  height={40}
+                />
+              </a>
+            </div>
+          </div>
         </div>
-        <span className="text-sm text-gray-500">
-          From setup to completion, MatMaster handles the details so you can
-          focus on wrestling.
-        </span>
       </div>
 
       <Divider />
